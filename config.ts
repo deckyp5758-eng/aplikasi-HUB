08edad0aacbc8f140d00ba5d137ffa0f00764f43
deckyp5758-eng <deckyp5758@gmail.com>
2026-08-26 13:14:50 +0000

    fix: point companion config to active Apps Script deployment

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxt5PDpBtv9vwtzj6Md273D61IU1Z2eYvdcGPGWZ6lG2KJxArcZIhz9Q3H-3l007VI/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbxEXMxyio1bpUiVn9bOlWqf9tHfnqcWBK3WUPEJgVDGcR1nqNSC3nnnatXNiMJm1fzQ/exec",
   SPREADSHEETS: {
     PENGIRIMAN: "1nCxvNqo7d0zRdLDAxWorFGOXOxfhr9S1x1man9O9xrw",
     ARMADA: "1nCxvNqo7d0zRdLDAxWorFGOXOxfhr9S1x1man9O9xrw",
