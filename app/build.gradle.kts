592a0986bb0831f76337a7e0809c6864c40c03c8
deckyp5758-eng <deckyp5758@gmail.com>
2026-09-17 23:51:16 +0700

    build: bump version to 1.5.0
    Update version code to 8 and version name to 1.5.0 for the upcoming release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.fleetodologger.pzmrvk"
     minSdk = 24
     targetSdk = 36
-    versionCode = 7
-    versionName = "1.4.0"
+    versionCode = 8
+    versionName = "1.5.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
